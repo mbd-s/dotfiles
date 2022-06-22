85da159c225c8522ae9608955e1a5d955369526a
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2022-06-22 10:12:27 +0200

    Refactor cluster-switching function

--- fish/functions/sc.fish
+++ fish/functions/sc.fish
@@ -1,10 +1,12 @@
 function sc -d 'Switch Kubernetes context'
-    set -f cluster_name $(aws eks list-clusters --query clusters | jq -r .[] | fzf --prompt="Select a cluster: ")
+    test "$AWS_PROFILE" = '' && echo $RED'No AWS profile set'$RESET_COLOR && return
+
+    set -f cluster_name $(aws --profile $AWS_PROFILE eks list-clusters --query clusters | jq -r .[] | fzf --prompt='Select a cluster: ')
 
     if test -n "$cluster_name"
         aws eks update-kubeconfig --name $cluster_name --region eu-central-1
-        echo $GREEN"Connected to the cluster $cluster_name in the $environment environment."$RESET_COLOR
+        echo $GREEN"Connected to the cluster $cluster_name with the $AWS_PROFILE AWS profile."$RESET_COLOR
     else
-        echo $RED"No cluster was selected, so the kubernetes context was not updated."$RESET_COLOR
+        echo $RED'No cluster was selected, so the Kubernetes context was not updated.'$RESET_COLOR
     end
 end
