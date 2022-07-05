b882d45790128bcc05a9e6feab188f75af62d738
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2022-07-05 09:47:29 +0200

    Ignore fisher files
    Updates to fisher and its plugins shouldn't propagate here. The `fish_plugins`
    file should be enough to keep fisher-managed packages in sync.

--- fish/completions/web-search.fish
+++
@@ -1,5 +0,0 @@
-set -l contexts google youtube bing yahoo duckduckgo startpage yandex github baidu ecosia goodreads qwant givero stackoverflow wolframalpha archive scholar nixpkgs nixoptions aur gpo searx brave
-
-for context in $contexts
-    complete -c web-search -f -n __fish_use_subcommand -a $context
-end
