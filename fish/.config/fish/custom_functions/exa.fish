efadb0759993d1c5517a0a3029db5d7a8899f9a4
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2023-11-15 11:16:40 +0100

    refactor: Make eza wrapper functions abbreviations
    They're just here for muscle memory anyway, and because `exa` feels
    a tiny bit easier to type than `eza`.

--- fish/.config/fish/custom_functions/exa.fish
+++
@@ -1,3 +0,0 @@
-function exa --description "eza with a few options (preserved for muscle memory)"
-    command eza --all --icons --color-scale --ignore-glob .DS_Store $argv
-end
--- fish/.config/fish/custom_functions/exall.fish
+++
@@ -1,3 +0,0 @@
-function exall --description "eza with a lot of options (preserved for muscle memory)"
-    command eza --long --binary --header --links --inode --group --created --modified --accessed --git --all --icons --time-style=long-iso --color-scale --ignore-glob .DS_Store $argv
-end
