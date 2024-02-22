c93e6cd189755c787a07e81e157beedeb6647a19
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2024-02-22 15:27:10 +0100

    Remove unneeded dependencies when updating Homebrew

--- fish/.config/fish/custom_functions/brewski.fish
+++ fish/.config/fish/custom_functions/brewski.fish
@@ -1,6 +1,7 @@
 function brewski --description "Update Homebrew and its packages"
     brew update
     brew upgrade --greedy
+    brew autoremove
     brew cleanup --prune=all
     brew doctor > /dev/null
 end
