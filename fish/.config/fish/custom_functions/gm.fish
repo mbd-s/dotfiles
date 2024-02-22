1961798d81f2fe79bb82c4f290a026a048fda7ef
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2024-02-22 15:34:55 +0100

    Improve whitespace output

--- fish/.config/fish/custom_functions/gm.fish
+++ fish/.config/fish/custom_functions/gm.fish
@@ -1,7 +1,7 @@
 function gm --description "Update various dependencies and reclaim disk space"
-    brewski && echo
-    docker_clean && echo
-    asdf_plugin_update && echo
+    brewski; echo
+    docker_clean; echo
+    asdf_plugin_update; echo
     mas upgrade 2> /dev/null
     fig doctor > /dev/null
 end
