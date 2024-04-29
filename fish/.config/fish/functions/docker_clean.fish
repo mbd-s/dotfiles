a1d64115a47d3a53949f5c78abe8d1066b23bfca
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2024-04-29 15:07:01 +0200

    Return early from Docker clean-up if it's not running

--- fish/.config/fish/functions/docker_clean.fish
+++ fish/.config/fish/functions/docker_clean.fish
@@ -1,5 +1,6 @@
 function docker_clean --description "Clean up Docker resources"
     echo $BOLD"Cleaning up Docker resources..."$RESET_COLOR
+    docker info &> /dev/null; or echo $RED"Docker isn't running"$RESET_COLOR && return 1
     for container in $(docker ps --all --quiet)
         docker stop $container
     end
