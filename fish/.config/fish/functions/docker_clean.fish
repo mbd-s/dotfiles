33d055790cdcdeed01a5afbb5639372569fc7f16
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2024-09-25 12:02:19 +0200

    Simplify Docker-cleanup scripts

--- fish/.config/fish/functions/docker_clean.fish
+++ fish/.config/fish/functions/docker_clean.fish
@@ -1,14 +1,10 @@
 function docker_clean --description "Clean up Docker resources"
     echo $BOLD"Cleaning up Docker resources..."$NORMAL
     docker info &> /dev/null; or echo $RED"Docker isn't running"$NORMAL && return 1
-    for container in $(docker ps --all --quiet)
+
+    for container in $(docker ps --quiet)
         docker stop $container
     end
 
-    docker container prune --force
-    docker volume prune --all --force
-    docker network prune --force
-    docker image prune --all --force
-    docker builder prune --all --force --verbose
-    docker system prune --all --force
+    docker system prune --all --volumes --force
 end
