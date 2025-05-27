55d0180e8d641967c7b87284def7f627a7a7fca8
Michael Silverberg <13588399+mbd-s@users.noreply.github.com>
2025-05-27 11:14:00 -0700

    Make Docker cleanup less aggressive

--- fish/.config/fish/functions/docker_clean.fish
+++ fish/.config/fish/functions/docker_clean.fish
@@ -1,10 +1,34 @@
 function docker_clean --description "Clean up Docker resources"
-    echo $BOLD"Cleaning up Docker resources..."$NORMAL
+    echo $YELLOW"Cleaning up unused Docker resources..."$NORMAL
     docker info &> /dev/null; or echo $RED"Docker isn't running"$NORMAL && return 1
 
-    for container in $(docker ps --quiet)
-        docker stop $container
-    end
+    echo
+    echo $YELLOW"Unused networks (not auto-removed):"$NORMAL
+    docker network ls --filter "dangling=true"
 
-    docker system prune --all --volumes --force
+    set --local pre_clean_df (docker system df)
+
+    echo
+    echo $YELLOW"Removing stopped containers"$NORMAL
+    docker container prune --force
+
+    echo
+    echo $YELLOW"Removing dangling images"$NORMAL
+    docker image prune --force
+
+    echo
+    echo $YELLOW"Removing build cache"$NORMAL
+    docker builder prune --force
+
+    echo
+    echo $YELLOW"Removing unused local volumes"$NORMAL
+    docker volume prune --force
+
+    echo
+    echo $YELLOW"Before cleanup:"$NORMAL
+    printf "%s\n" $pre_clean_df
+
+    echo
+    echo $YELLOW"After cleanup:"$NORMAL
+    docker system df
 end
